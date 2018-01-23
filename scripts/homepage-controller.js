a55a9e11591b585da61b228f8d450effa5fa024e
Jefferson <jeff.adams@hotmail.com>
2018-01-22 20:36:09 -0500

    made minor changes - edited input. starting output.

--- scripts/homepage-controller.js
+++ scripts/homepage-controller.js
@@ -1,9 +1,9 @@
 console.log("controller loaded");
 
-var budgetCalc = angular.module('budgetCalc');
-
-budgetCalc.controller('link', function($scope){
-
-  $scope.test = "i havent eaten a banana in 6 months";
-
-});
+// var budgetCalc = angular.module('budgetCalc');
+//
+// budgetCalc.controller('link', function($scope){
+//
+//   $scope.test = "i havent eaten a banana in 6 months";
+//
+// });
---
+++ scripts/service.js
@@ -0,0 +1,11 @@
+console.log("service is loaded");
+
+// check to see if weekly or monthly is selected for inputs
+// var inputType = document.getElementsByName("WorM")[0].value;
+//
+//
+// if (inputType.value=="weekly"){
+//   console.log("weekly selected");
+// } else if (inputType.value=="monthly"){
+//   console.log("monthly selected");
+// }
